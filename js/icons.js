0cf0e169b013fb3139e538b55f355cac6dde8cb9
JorgeLuix <tec.jorge.29@gmai.com>
2023-03-25 18:26:42 +0100

    funzione color random e applicando stitli

--- js/icons.js
+++ js/icons.js
@@ -5,117 +5,131 @@ const icons = [
         prefix: 'fa-',
         type: 'animal',
         family: 'solid',
-        color: 'orange'
+        color: ''
     },
     {
         name: 'crow',
         prefix: 'fa-',
         type: 'animal',
         family: 'solid',
-        color: 'orange'
+        color: ''
     },
     {
         name: 'dog',
         prefix: 'fa-',
         type: 'animal',
         family: 'solid',
-        color: 'orange'
+        color: ''
     },
     {
         name: 'dove',
         prefix: 'fa-',
         type: 'animal',
         family: 'solid',
-        color: 'orange'
+        color: ''
     },
     {
         name: 'dragon',
         prefix: 'fa-',
         type: 'animal',
         family: 'solid',
-        color: 'orange'
+        color: ''
     },
     {
         name: 'horse',
         prefix: 'fa-',
         type: 'animal',
         family: 'solid',
-        color: 'orange'
+        color: ''
     },
     {
         name: 'hippo',
         prefix: 'fa-',
         type: 'animal',
         family: 'solid',
-        color: 'orange'
+        color: ''
     },
     {
         name: 'fish',
         prefix: 'fa-',
         type: 'animal',
         family: 'solid',
-        color: 'orange'
+        color: ''
     },
     {
         name: 'carrot',
         prefix: 'fa-',
         type: 'vegetable',
         family: 'solid',
-        color: 'green'
+        color: ''
     },
     {
         name: 'apple-alt',
         prefix: 'fa-',
         type: 'vegetable',
         family: 'solid',
-        color: 'green'
+        color: ''
     },
     {
         name: 'lemon',
         prefix: 'fa-',
         type: 'vegetable',
         family: 'solid',
-        color: 'green'
+        color: ''
     },
     {
         name: 'pepper-hot',
         prefix: 'fa-',
         type: 'vegetable',
         family: 'solid',
-        color: 'green'
+        color: ''
     },
     {
         name: 'user-astronaut',
         prefix: 'fa-',
         type: 'user',
         family: 'solid',
-        color: 'blue'
+        color: ''
     },
     {
         name: 'user-graduate',
         prefix: 'fa-',
         type: 'user',
         family: 'solid',
-        color: 'blue'
+        color: ''
     },
     {
         name: 'user-ninja',
         prefix: 'fa-',
         type: 'user',
         family: 'solid',
-        color: 'blue'
+        color: ''
     },
     {
         name: 'user-secret',
         prefix: 'fa-',
         type: 'user',
         family: 'solid',
-        color: 'blue'
+        color: ''
     }
 ];
+//funzione per generare colori randomicamente.
+//nel array di oggeti prima,dobbiammo svuotare il contenuto asegnato a "color".
+function getRandomColor() {
+    const hexIcons = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+      color += hexIcons[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
+  
+  // assegnare un colore casuale a ciascuna icona
+  icons.forEach(icon => {
+    icon.color = getRandomColor();
+  });
 
 //funzione per creare le cards
-
 function creatCard(filterIcons) {
     const card = document.querySelector('.row');
     card.innerHTML = '';
@@ -123,7 +137,7 @@ function creatCard(filterIcons) {
 
 filterIcons.forEach(element => {
     const tpl = ` <div class="col-12 col-sm-6 col-md-4 col-lg-3">
-    <div class="card">
+    <div class="card icon-card">
         <div class="card-body d-flex flex-column 
         justify-content-center align-items-center fs-3 py-4"
         style="color:${element.color}">
@@ -146,7 +160,7 @@ function filterIcons(icons, type) {
 // funzione per aggiungere select alla pagina
 function addSelect() {
     const select = document.getElementById('type-select');
-    const types = ['all', 'animal', 'vegetable', 'user'];
+    const types = ['animal', 'vegetable', 'user'];
 
     types.forEach(type => {
         const option = document.createElement('option');
@@ -158,7 +172,7 @@ function addSelect() {
     select.addEventListener('change', function() {
         const selectedType = this.value;
         let filteredIcons;
-        if (selectedType === "all") {        
+        if (selectedType === "all" || selectedType === icons.type) {        
             filteredIcons = icons;
         } else {
             filteredIcons = filterIcons(icons, selectedType);
